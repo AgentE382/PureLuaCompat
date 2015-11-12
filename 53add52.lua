56cf514eeee6b1a9319f4afbba29f7181949490d
AgentE382 <AgentE382@gmail.com>
2015-11-12 18:05:33 -0500

    Add Development Functions

--- 53add52.lua
+++ 53add52.lua
@@ -1,2 +1,8 @@
+-- Aux
+function printf(...)
+  local function wrapper(...) io.write(string.format(...)) end
+  local status, result = pcall(wrapper, ...)
+  if not status then error(result, 2) end
+end
 
 return -- option processing function
